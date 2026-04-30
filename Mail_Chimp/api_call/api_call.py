e6bbfaf02ed9aff2f7cd9ed985140dc9a47b08c6
TKZ <tkzellers@gmail.com>
2026-04-30 06:23:33 -0400

    mc api call

--- Mail_Chimp/api_call/api_call.py
+++ Mail_Chimp/api_call/api_call.py
@@ -1,12 +1,13 @@
 import os
 from mailchimp_marketing import Client
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
 # Read .env file
-api_key = os.getenv('MAILCHIMP_API_KEY')
-server_prefix = os.getenv('MAILCHIMP_SERVER_PREFIX')
+api_key = os.getenv('MC_API_KEY')
+server_prefix = os.getenv('MC_SERVER_PREFIX')
 
 mailchimp = Client()
 mailchimp.set_config({
@@ -16,3 +17,8 @@ mailchimp.set_config({
 
 response = mailchimp.ping.get()
 print(response)
+
+campaigns = mailchimp.campaigns.list()
+
+with open('campaigns.json', 'w') as f:
+    json.dump(campaigns, f, indent=4)
